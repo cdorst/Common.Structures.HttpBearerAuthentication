4eb62ffa11d0216deed2b3dd5f7f8a67c256df90
Christopher Dorst <chris@dorst.email>
2018-01-12 12:09:45 -0600

    Add BearerAuthenticationCredentials

---
+++ Common.Structures.HttpBearerAuthentication/BearerAuthenticationCredentials.cs
@@ -0,0 +1,17 @@
+﻿using Common.Structures.HttpAuthentication;
+
+namespace Common.Structures.HttpBearerAuthentication
+{
+    public class BearerAuthenticationCredentials : HttpAuthentication
+    {
+        private const string Bearer = nameof(Bearer);
+        private readonly string _secret;
+
+        public BearerAuthenticationCredentials(string secret) : base(Bearer)
+        {
+            _secret = secret;
+        }
+
+        protected override string Value => _secret;
+    }
+}
